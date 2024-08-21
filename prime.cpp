efdf4ffd38f9e0a0e320ea4e6b5ef22a6d33e4b0
Arpan Gupta <arpan.gupta@lpu.in>
2024-08-21 14:42:56 +0530

    addedc ode

--- prime.cpp
+++ prime.cpp
@@ -19,3 +19,24 @@ int main(){
 // 1. we are checking from 2 to sqrt(n) because if a number is not prime then it will have a factor less than or equal to sqrt(n)
 // 2. if a number is not prime then it will have a factor less than or equal to sqrt(n) so we are checking till sqrt(n) only
 // 3. if a number is not prime then it will have a factor less than or equal to sqrt(n) so we are checking till sqrt(n) only
+
+//more Optimised solution
+bool isprime(int n){
+    if(n==1) return false;
+    if(n==2 || n==3) return true;
+    if(n%2==0 || n%3==0) return false;
+    for(int i = 5;i*i<=n;i+=6){
+        if(n%i==0 || n%(i+2)==0) return false;
+    }
+    return true;
+}
+int main(){
+    bool x = isprime(7);
+    if(x) cout<<"prime";
+    else cout<<"not prime";
+}
+//explanation
+// 1. we are checking from 5 to sqrt(n) because if a number is not prime then it will have a factor less than or equal to sqrt(n)
+// 2. if a number is not prime then it will have a factor less than or equal to sqrt(n) so we are checking till sqrt(n) only
+// 3. if a number is not prime then it will have a factor less than or equal to sqrt(n) so we are checking till sqrt(n) only
+// 4. we are checking for 2 and 3 separately because we are checking for i+2 in the loop so we are checking for 2 and 3 separately
